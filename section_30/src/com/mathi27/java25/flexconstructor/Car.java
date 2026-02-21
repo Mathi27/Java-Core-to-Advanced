8466c3cd4e171af9ca141809da6290ce69ba6113
Mathi27 <mathiyuvarajan.dev@gmail.com>
2026-02-21 23:37:53 +0530

    flexconstructor fix

--- section_30/src/com/mathi27/java25/flexconstructor/Car.java
+++ section_30/src/com/mathi27/java25/flexconstructor/Car.java
@@ -5,7 +5,7 @@ public class Car extends Vehicle {
     Car(int speed) {
         if (speed < 0) throw new IllegalArgumentException("Invalid speed");
         if (speed > 200) throw new IllegalArgumentException("Car too fast");
-        super(speed); // Forced first call
+        super(speed);  // First call
     }
 
     private static int verifySpeed(int speed) {
